101c0ea6e4e9a91e28140523308cf12421e91540
Tom <tom.s.jackson090@gmail.com>
2020-12-16 15:59:23 +0000

    binSearch all test cases passed

--- binarySearch/bin.js
+++ binarySearch/bin.js
@@ -3,8 +3,18 @@ let size3 = [1, 2, 3];
 let size4 = [1, 2, 3, 4];
 
 const fn = (num, arr) => {
-  let i;
-  return i;
+  if (arr.length === 0) return -1;
+
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === num) return mid;
+    if (num > arr[mid]) left = mid + 1;
+    if (num < arr[mid]) right = mid - 1;
+  }
+  return -1;
 };
 
 module.exports = {
