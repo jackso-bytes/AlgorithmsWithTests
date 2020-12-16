f6b82880197245235276479bf22145cfbfb653a3
Tom <tom.s.jackson090@gmail.com>
2020-12-16 11:10:44 +0000

    discounts: Initial failing tests for #main and #getPrice

---
+++ discounts/discount.js
@@ -0,0 +1,22 @@
+let testCase1 = [
+  { name: "chocolate", price: "£50", quantity: 2, discount: "2 for 45" },
+  { name: "marmalade", price: "£50", quantity: 3, discount: "3 for 100" },
+  { name: "rice", price: "£10", quantity: 1, discount: null },
+];
+
+class Discount {
+  main() {}
+
+  getPrice(price) {
+    return price;
+  }
+
+  applyDiscount(price, quantity, discount) {
+    return price;
+  }
+}
+
+module.exports = {
+  C: discount,
+  i: testCase1,
+};
---
+++ discounts/discount.test.js
@@ -0,0 +1,39 @@
+const imports = require("./discount");
+const { C, i } = imports;
+
+describe("#Main", () => {
+  const methods = new c();
+
+  test("return null if not passed array of objects", () => {
+    expect(methods.main(1)).toBe(
+      "You must call #main with an array of objects"
+    );
+    expect(methods.main("hello world")).toBe(
+      "You must call #main with an array of objects"
+    );
+    expect(methods.main({})).toBe(
+      "You must call #main with an array of objects"
+    );
+    expect(methods.main([])).toBe(
+      "You must call #main with an array of objects"
+    );
+  });
+
+  /*test("are final prices correct?", () => {
+    expect(methods.main(i)).toBe("£155");
+  });*/
+});
+
+describe("#getPrice", () => {
+  test("return null if passed an invalid input", () => {
+    const methods = new C();
+    expect(methods.getPrice("hello world")).toBe(null);
+    expect(methods.getPrice(["£20", "£30"])).toBe(null);
+    expect(methods.getPrice(30)).toBe(null);
+    expect(methods.getPrice({ key: "£20" })).toBe(null);
+  });
+
+  test("returns correct value as a integer", () => {
+    expect(methods.getPrice("£30")).toBe(30);
+  });
+});
