462d46e1c38d81326327dae1e7b0438b48362616
Tom <tom.s.jackson090@gmail.com>
2021-01-08 09:31:11 +0000

    parentheses: tests passing for valid parentheses and empty string

--- validParentheses/parentheses.js
+++ validParentheses/parentheses.js
@@ -1,14 +1,32 @@
-const vp = (i) => {
+const vp = (inp) => {
   //handle edge case that we're passed something that isn't a str
-  if (typeof i !== "string") {
+  if (typeof inp !== "string") {
     return false;
   } else if (
-    i.includes("{") === false &&
-    i.includes("(") === false &&
-    i.includes("[") === false
+    inp.includes("{") === false &&
+    inp.includes("(") === false &&
+    inp.includes("[") === false
   ) {
     return false;
   }
+
+  let stack = [];
+
+  let vp = {
+    "[": "]",
+    "(": ")",
+    "{": "}",
+  };
+
+  for (let i = 0; i < inp.length; i++) {
+    if (vp[inp[i]]) {
+      stack.push(inp[i]);
+    } else if (vp[stack.pop()] !== inp[i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 module.exports = {
--- validParentheses/parentheses.test.js
+++ validParentheses/parentheses.test.js
@@ -1,28 +1,31 @@
-const imp = require("./parentheses");
+const inp = require("./parentheses");
 
 describe("test suite for true cases", () => {
   test("return true when passed a set of mixed parentheses closed in the correct order", () => {
-    expect(imp.f("[{()}]")).toBe(true);
+    expect(inp.f("[{()}]")).toBe(true);
   });
   test("return true passed an empty string", () => {
-    expect(imp.f("")).toBe(true);
+    expect(inp.f("")).toBe(true);
   });
 });
 
 describe("test suite for false cases", () => {
   test("return false when no parentheses are found", () => {
-    expect(imp.f("hello world")).toBe(false);
+    expect(inp.f("hello world")).toBe(false);
   });
   test("return false when passed a no", () => {
-    expect(imp.f(123)).toBe(false);
+    expect(inp.f(123)).toBe(false);
   });
   test("return false when passed a bool", () => {
-    expect(imp.f(true)).toBe(false);
+    expect(inp.f(true)).toBe(false);
   });
   test("return false when passed a datastructure", () => {
-    expect(imp.f([])).toBe(false);
+    expect(inp.f([])).toBe(false);
   });
   test("return false when passed an invalid set of parentheses", () => {
-    expect(imp.f("[{(}]")).toBe(false);
+    expect(inp.f("[{(}]")).toBe(false);
+  });
+  test("return false when passed junk string that happens to have parentheses", () => {
+    expect(inp.f("hello[]hsduhsd")).toBe(false);
   });
 });
