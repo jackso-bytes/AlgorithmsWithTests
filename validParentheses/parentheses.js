e876cfe06dadc3cbc1f2d72c582dc46ee46457f8
Tom <tom.s.jackson090@gmail.com>
2021-01-08 09:14:13 +0000

    parentheses: passing tests for type input edge cases

--- validParentheses/parentheses.js
+++ validParentheses/parentheses.js
@@ -1,4 +1,15 @@
-const vp = (i) => {};
+const vp = (i) => {
+  //handle edge case that we're passed something that isn't a str
+  if (typeof i !== "string") {
+    return false;
+  } else if (
+    i.includes("{") === false &&
+    i.includes("(") === false &&
+    i.includes("[") === false
+  ) {
+    return false;
+  }
+};
 
 module.exports = {
   f: vp,
