912fbef12bf1069dbf6f7beb359c2bb32f3c6bdf
Tom <tom.s.jackson090@gmail.com>
2021-01-19 19:39:11 +0000

    rotateImage: solution

--- rotateImage/rotate.js
+++ rotateImage/rotate.js
@@ -1,5 +1,21 @@
-const f = () => {
-  return null;
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const f = (arr) => {
+  arr.reverse();
+  arr.forEach((element, index) => {
+    for (let i = index + 1; i < element.length; i++) {
+      let store = arr[index][i];
+      arr[index][i] = arr[i][index];
+      arr[i][index] = store;
+    }
+  });
+  return arr;
 };
+
+f(arr);
 
 module.exports = f;
--- rotateImage/rotate.test.js
+++ rotateImage/rotate.test.js
@@ -1,7 +1,17 @@
 let f = require("./rotate");
 
-describe("blank test", () => {
-  test("hello world", () => {
-    expect(f("hello world")).toBe(null);
+describe("", () => {
+  test("basic test for if matrix has been rotated 90 deg", () => {
+    expect(
+      f([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ])
+    ).toStrictEqual([
+      [7, 4, 1],
+      [8, 5, 2],
+      [9, 6, 3],
+    ]);
   });
 });
