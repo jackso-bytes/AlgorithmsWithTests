b4fd2fc9bb3237b12ce02b900432464eea7a833b
Tom <tom.s.jackson090@gmail.com>
2021-01-22 16:11:43 +0000

    3Sum: passing tests with no repetition in input

--- ThreeSum/three.js
+++ ThreeSum/three.js
@@ -1,12 +1,25 @@
 const f = (arr) => {
+  arr.sort();
   let output = [];
-  for (let i = 0; i < arr.length - 3; arr++) {
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
     let right = arr.length - 1;
-    let mid = Math.floor((left + right) / 2);
-    if (arr[i] + arr[mid] + arr[right] === 0) {
-      output.push([arr[i], arr[mid], arr[right]]);
+    if (i > 0 && arr[i] === arr[i - 1]) continue;
+    while (left < right) {
+      let total = arr[i] + arr[left] + arr[right];
+      if (total === 0) {
+        output.push([arr[i], arr[left], arr[right]]);
+        left = i + 1;
+        right = arr.length - 1;
+      }
+      if (total > 0) {
+        right--;
+      } else {
+        left++;
+      }
     }
   }
+  console.log(output);
   return output;
 };
 
--- ThreeSum/three.test.js
+++ ThreeSum/three.test.js
@@ -3,8 +3,15 @@ const f = require("./three");
 describe("pass cases", () => {
   test("simple pass", () => {
     expect(f([1, 2, -2, -1, 3, -3])).toStrictEqual([
-      [1, 2, -3],
-      [-2, -1, 3],
+      [-1, -2, 3],
+      [-3, 1, 2],
+    ]);
+  });
+
+  test("pass with clusters", () => {
+    expect(f([1, 1, 2, 2, -2, -1, 3, -3])).toStrictEqual([
+      [-1, -2, 3],
+      [-3, 1, 2],
     ]);
   });
 });
