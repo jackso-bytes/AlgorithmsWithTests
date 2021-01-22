6139f43dccc326f86a336175c25dafc031b1017e
Tom <tom.s.jackson090@gmail.com>
2021-01-22 12:30:51 +0000

    threeSum: better pointers and outer loop

--- ThreeSum/three.js
+++ ThreeSum/three.js
@@ -1,8 +1,12 @@
 const f = (arr) => {
   let output = [];
-  let left = 0;
-  let right = arr.length - 1;
-  while (left < right) {}
+  for (let i = 0; i < arr.length - 3; arr++) {
+    let right = arr.length - 1;
+    let mid = Math.floor((left + right) / 2);
+    if (arr[i] + arr[mid] + arr[right] === 0) {
+      output.push([arr[i], arr[mid], arr[right]]);
+    }
+  }
   return output;
 };
 
